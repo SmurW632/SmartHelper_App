848046cb583d876f5097c39f4e622df364ed72a6
Mikhail <smurw.developer@gmail.com>
2024-06-13 23:17:05 +1000

    доб. репозитории таблицы SupportMeasure

---
+++ SmartHelper.Server/Data/Repositories/NPARepository.cs
@@ -0,0 +1,27 @@
+﻿using SmartHelper.Server.Models.Support_measures;
+
+namespace SmartHelper.Server.Data.Repositories
+{
+    public class NPARepository : IRepository<NPA>
+    {
+        public void Delete(NPA entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NPA GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(NPA entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(NPA entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SmartHelper.Server/Data/Repositories/SupportMeasureRepository.cs
@@ -0,0 +1,27 @@
+﻿using SmartHelper.Server.Models.Support_measures;
+
+namespace SmartHelper.Server.Data.Repositories
+{
+    public class SupportMeasureRepository : IRepository<SupportMeasure>
+    {
+        public void Delete(SupportMeasure entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SupportMeasure GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(SupportMeasure entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(SupportMeasure entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
