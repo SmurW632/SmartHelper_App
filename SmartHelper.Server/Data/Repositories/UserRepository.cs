ca5cba4d9773e04a4f36cdb2cd59027d030747e8
Mikhail <smurw.developer@gmail.com>
2024-06-13 20:34:04 +1000

    добавлен репозиторий user

---
+++ SmartHelper.Server/Data/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using SmartHelper.Server.Models.Auth;
+
+namespace SmartHelper.Server.Data.Repositories
+{
+    public class UserRepository : IRepository<AuthUser>
+    {
+        public void Delete(AuthUser entity)
+        {
+            
+        }
+
+        public AuthUser GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(AuthUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(AuthUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
